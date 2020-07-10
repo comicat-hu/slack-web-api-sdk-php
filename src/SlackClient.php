b89f0111e8a80c40d35f274dac13f7559008c51e
comicat-hu <senline39518@gmail.com>
2020-07-10 18:51:16 +0800

    Fixed pass endpoint parameters bugs

--- src/SlackClient.php
+++ src/SlackClient.php
@@ -6,17 +6,26 @@ use Comicat\Slack\ClientFactory;
 
 class SlackClient
 {
+    protected $token;
+
     protected $client;
 
     public function __construct(string $token)
     {
+        $this->token = $token;
         $this->client = ClientFactory::create($token);
         return $this->client;
     }
 
+    /**
+     * handle issue https://github.com/janephp/janephp/issues/278#issuecomment-607745381
+     */
     public function __call($function, $args)
     {
-        // https://github.com/janephp/janephp/issues/278#issuecomment-607745381
-        return $this->client->$function($args[0], ['token' => '']);
+        $numberOfMethodParameters = (new \ReflectionObject($this->client))->getMethod($function)->getNumberOfParameters();
+        if ($numberOfMethodParameters == 3 && !isset($args[1]['token'])) {
+            $args[1]['token'] = $this->token;
+        }
+        return $this->client->$function(...$args);
     }
 }
